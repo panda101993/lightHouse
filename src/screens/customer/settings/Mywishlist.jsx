b9c36a5a80e4ac0693fcc33734eeaeed6785f498
Rana_Pandey <re-ranapandey@mobiloitte.com>
2020-04-06 20:07:15 +0530

    HTML integration

---
+++ src/screens/customer/settings/Mywishlist.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
+import { GlobalValidations } from '../../../components/GlobalValidations';
+import { GlobalButtonLinks } from '../../../components/GlobalButtonLinks';
+import { Link } from 'react-router-dom';
+
+export default class Mywishlist extends Component {
+    render() {
+        return (
+            <div>
+                {/* <section className="center-form"> */}
+                <body>
+                    <Header />
+                   
+
+                    <Footer />
+
+                </body>
+
+
+
+            </div>
+        )
+    }
+}
