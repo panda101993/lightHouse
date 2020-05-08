ccf3bd579f600d85abd3d4a5aabde4627fa4f1b3
shivam singh <re-shivamsingh@mobiloitte.com>
2020-05-08 22:02:37 +0530

    worker js file work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navigation from './navigation/Navigation';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+  Switch,
+} from 'react-router-dom'
+import CreateWebpageInitialSignupProcess from './screens/create_webpage(initial_signup_process)/CreateWebpageInitialSignupProcess';
 function App() {
   return (
     <div >
-   <Navigation/>
+   {/* <Navigation/>  */}
+    <Router>
+      <Route exact path="/" component={CreateWebpageInitialSignupProcess}></Route>
+    </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -16,4 +16,6 @@ ReactDOM.render(<Provider store={Store}>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
+//serviceWorker.register(); 
+serviceWorker.unregister();
