b0b2f7019a10238ee367bbaa516611a04f0fd567
Mohd Danish <re-mohddanish@mobiloitte.com>
2020-06-22 20:05:01 +0530

    footer api integration complete

--- src/screens/Contact_us.jsx
+++ src/screens/Contact_us.jsx
@@ -3,6 +3,8 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { Modal, ModalBody } from "reactstrap"
 import { Link } from "react-router-dom"
+import Apirequest from '../api/Apirequest'
+import {validateEmail,validateName} from '../utils/validation/Validation';
 
 export default class Contact_us extends Component {
 
@@ -11,9 +13,27 @@ export default class Contact_us extends Component {
 
       this.state = {
          name: '',
+         nameStatus: false,
+         nameErrorMessage: '',
+         nameErrorStatus:false,
+
          email: '',
+         emmailStatus: false,
+         emailErrorStatus: false,
+         emailErrorMessage:'',
+
          subject: '',
+         subjectStatus: false,
+         sujectErrorMessage:'',
+         sujectErrorStatus:false,
+
          description: '',
+         descriptionStatus: false,
+         fielderr: '',
+         error:'',
+         
+         
+
          // otp: "",
          // otpErrorMessage: "",
          // otpStatus: false,
@@ -21,65 +41,82 @@ export default class Contact_us extends Component {
 
       }
    }
-   validateName(name) {
-      var nameRegex = /^[a-zA-Z ]+$/;
-      var name = name.trim();
-      if (name == "" || name == undefined || name == null) {
-         return { status: false, error: "*Please enter your name." };
-      } else if (!nameRegex.test(name)) {
-         return { status: false, error: "*Please enter atleast 2 characters." };
-      } else if (name.length < 2) {
-         return { status: false, error: "*Please enter atleast 2 characters." };
-      } else {
-         return { status: true, error: "" };
-      }
-   }
-   validateName(subject) {
-      var nameRegex = /^[a-zA-Z ]+$/;
-      var name = name.trim();
-      if (name == "" || name == undefined || name == null) {
-         return { status: false, error: "*Please enter your subject." };
-      } else if (!nameRegex.test(name)) {
-         return { status: false, error: "*Please enter atleast 2 characters." };
-      } else if (name.length < 2) {
-         return { status: false, error: "*Please enter atleast 2 characters." };
-      } else {
-         return { status: true, error: "" };
-      }
-   }
+  
 
-   validateEmail(email) {
-      var emailRegex = /^[A-Z0-9_-]+([\.][A-Z0-9_]+)*@[A-Z0-9-]+(\.[a-zA-Z]{2,3})+$/i;
-      email = email.trim();
-      if (email == "" || email == undefined || email == null) {
-         return { status: false, error: "*Please enter the email." };
-      } else if (!emailRegex.test(email)) {
-         return { status: false, error: "*Please enter valid Email Address." };
-      } else {
-         return { status: true, error: "" };
+
+  
+   inputHandler = (e, type) => {
+      switch (type) {
+         case "name":
+            this.setState({ name: e.target.value}, () => this.handleValidation("name"))
+            break;
+         case "email":
+            this.setState({ email: e.target.value},  () => this.handleValidation("email"))
+            break;
+         case "subject":
+            this.setState({ subject: e.target.value, subjectStatus: true })
+            break;
+         case "description":
+            this.setState({ description: e.target.value, descriptionStatus: true })
+            break;
+         default:
+            {
+               console.log("error")
+            }
+            break;
+
       }
    }
-   // inputHandler = (event, type) => {
-   //    switch (type) {
-   //       case name:
-   //          this.setState({ name: event.target.value })
-   //          break;
-   //       case email:
-   //          this.setState({ email: event.target.value })
-   //          break;
-   //       case subject:
-   //          this.setState({ subject: event.target.value })
-   //          break;
-   //       case description:
-   //          this.setState({ description: event.target.value })
-   //          break;
-   //       default:
-   //          break;
-   //    }
-   // }
-   // submitHandler = () =>{
+   handleValidation = (key) => {
+      switch (key) {
+         case ("name"):
+            var data = validateName(this.state.name)
+            console.log("this is data of name", data)
+            this.setState({ nameErrorMessage: data.error, nameErrorStatus: data.status }, () => console.log("error", this.state))
+            break;
+         case ("email"):
+            var data = validateEmail(this.state.email)
+            console.log("this is data of Email", data)
+            this.setState({ emailErrorMessage: data.error, emailErrorStatus: data.status }, () => console.log("error", this.state))
+            break;
+         // case ("suject"):
+         //    var data = validateSuject(this.state.subject)
+         //    console.log("this is data of name", data)
+         //    this.setState({ sujectErrorMessage: data.error, sujectErrorStatus: data.status }, () => console.log("error", this.state))
+         //    break;
 
-   // }
+         default:
+            break;
+      }
+   }
+   submitHandler = () => {
+      if (this.state.name == "" || this.state.email == "" || this.state.subject == "" || this.state.description == "") {
+         this.setState({ fielderr: "Please enter all fields" })
+      }
+      else {
+         this.setState({ fielderr: "" })
+         var requestData = {
+            "name": this.state.name,
+
+            "email": "no-aliahmad@mobiloitte.com",
+            "subject": this.state.subject,
+            "description": this.state.description,
+
+         }
+         console.log("data", requestData)
+         Apirequest(requestData, "/user/contactUs", "POST") 
+            .then((resp) => {
+               console.log("wxyz==>", resp);
+
+            })
+         
+            .catch(e => { console.log(e) })
+
+
+           
+      }
+      
+   }
 
 
    render() {
@@ -112,20 +149,28 @@ export default class Contact_us extends Component {
                         <form>
                            <div class="frmcent">
                               <div class="form-group">
+                             
                                  <label>Name</label>
-                                 <input type="text" class="form-control shpnm" aria-describedby="emailHelp" placeholder="Name" onChange={(event) => this.inputHandler(event)} />
+                               
+                                 <input type="text" class="form-control shpnm" aria-describedby="emailHelp" placeholder="Name" onChange={(e) => this.inputHandler(e, "name")} />
+                                 
+                                {this.state.nameErrorMessage}
+                             
                               </div>
                               <div class="form-group">
                                  <label>Email ID</label>
-                                 <input type="email" class="form-control shpnm" placeholder="Email Id" onChange={(event) => this.inputHandler(event)} />
+                                 <input type="email" class="form-control shpnm" placeholder="Email Id" onChange={(e) => this.inputHandler(e, "email")} />
+                                 {this.state.emailErrorMessage}
                               </div>
                               <div class="form-group">
                                  <label>Subject</label>
-                                 <input type="text" class="form-control shpnm" aria-describedby="emailHelp" placeholder="Subject" onChange={(event) => this.inputHandler(event)} />
+                                 <input type="text" class="form-control shpnm" aria-describedby="emailHelp" placeholder="Subject" onChange={(e) => this.inputHandler(e, "subject")} />
+                                 {/* {this.state.subjectErrorMessage} */}
                               </div>
                               <div class="form-group">
                                  <label>Description</label>
-                                 <textarea class="form-control shpnm" rows="5" placeholder="Description"></textarea>
+                                 <textarea class="form-control shpnm" rows="5" placeholder="Description" onChange={(e) => this.inputHandler(e, "description")} ></textarea>
+                                 {/* {this.state.descriptionErrorMessage} */}
                               </div>
                               <div class="save-cancel">
                                  <button type="button" class="btn btn-save bt" onclick="window.location.href = 'index.html';" >Cancel</button>
