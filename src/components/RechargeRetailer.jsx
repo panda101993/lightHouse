ed31b3675dcea68762aa944e259d302cca6de770
Hitesh Panwar <hiteshpanwar081997@gmail.com>
2020-06-29 22:11:05 +0530

    Top UP Recharge credit Api

--- src/components/RechargeRetailer.jsx
+++ src/components/RechargeRetailer.jsx
@@ -1,17 +1,66 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-export default class RechargeRetailer extends Component {
+import { connect } from "react-redux";
+import ToasterFunction from '../components/ToasterFunc';
+import ApiRequest from '../api/Apirequest';
+ class RechargeRetailer extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
 
             modalStatus: false,
+            unitCreditCost: null
 
         }
     }
+    componentDidMount(){
+     
+        let configObj = {
+            token: this.props.applicationData.token,
+            configType: "RETAILER"
+         }
+         try {
+            ApiRequest(configObj, '/configuration/configurations/RETAILER', 'GET', this.props.applicationData.token)
+               .then((resp) => {
+                  console.log('/configuration/configurations/{configType}', resp);
+   
+                  switch (resp.status) {
+                     case (200):
+                        {
+                           if (resp.data.responseCode == 200) {
+                              ToasterFunction("info", "Data is saved successfully");
+                              console.log("/configuration/configurations/RETAILER", resp.data.result)
+                             this.setState({unitCreditCost : resp.data.result.unitCreditCost})
+                           }
+   
+                           else if (resp.data.responseCode == 404) {
+                              ToasterFunction("info", "Data not found, internal server error");
+                           }
+                           else if (resp.data.responseCode == 500) {
+                              ToasterFunction("error", "Internal Server Error");
+                           }
+                        }
+                     case (900): {
+                        if (resp.status == 900) {
+                           ToasterFunction("error", "Please check your internet connection")
+                        }
+                     }
+                  }
+   
+               })
+         } catch (error) {
+            console.log('errorresponse', error);
+            // ToasterFunction("error", "Network error, please contact the administrator");
+         }
+    }
+
+
+
+
+
+
 
     render() {
         return (
@@ -56,14 +105,22 @@ export default class RechargeRetailer extends Component {
                                                     </li>
                                                 </ul>
                                                 <ul class="rec-credit">
-                                                    <li>Recharge Rs 500.00 You will receive 100 credits</li>
+        <li>{this.state.unitCreditCost?(`Recharge Rs 500.00 You will receive ${500/this.state.unitCreditCost} credits`):''}</li>
                                                 </ul>
                                             </div>
                                         </div>
-
-
             </div>
         )
     }
 }
 
+const mapStateToProps = state => {
+    console.log("stateLogin-------", state)
+    return {
+       applicationData: state.AuthReducer.userData,
+ 
+    }
+ 
+ }
+
+ export default connect(mapStateToProps)(RechargeRetailer);
