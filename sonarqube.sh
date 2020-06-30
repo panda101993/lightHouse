ddecbccaee55a24a57496c147150f420b5afba2e
Administrator <admin@example.com>
2020-06-30 20:09:37 +0000

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /home/administrator/sonar-scanner-4.3.0.2102/bin/sonar-scanner \
-  -Dsonar.projectKey=bitcoin-lighthouse-progressive-web-application-20013649-react-re-mohaminakhtar \
+  -Dsonar.projectKey=lighthouse-progressive-web-application-20013649-react-re-mohaminakhtar \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://172.16.0.220:9000 \
   -Dsonar.login=32cf9325138f97409198a1a1373cd27f6c9cdded \
